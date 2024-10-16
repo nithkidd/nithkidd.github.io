f1fae5a6a6e43c0b26c13446f9194fd767968622
nithkidd <123559135+nithkidd@users.noreply.github.com>
2024-10-17 01:10:45 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,23 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
+const menuLinks = document.querySelectorAll('#navbar a');
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    // Prevent default behavior for non-hash links
+    if (this.getAttribute('href').startsWith('#')) {
+      event.preventDefault(); // Prevent default only for internal links
+      menuLinks.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+      
+      // Smooth scrolling to the target section
+      const targetId = this.getAttribute('href');
+      document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
